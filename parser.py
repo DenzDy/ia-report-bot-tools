d4b6d1c6bd00d7e170b5ab6dc4ad9fc56a41ee7c
DenzDy <dydenzellrobyn0527@gmail.com>
2026-01-16 01:26:16 +0800

    feat: create BaseModel concretions for report data

--- parser.py
+++ parser.py
@@ -1,1 +1,16 @@
 from pydantic import BaseModel
+from typing import List
+
+class DetailsTable(BaseModel):
+    observation: str
+    risk: str
+    risk_rating: str
+    recommendation: str
+    status: str
+class ReportData(BaseModel):
+    report_title : str
+    executive_summary : str
+    details: List[DetailsTable]
+    recommendations: List[str]
+    management_action_plan: List[str]
+     
